113c013947c55810ecd5d3bf61747fb2a805d717
mTsBucy1 <85930053+mTsBucy1@users.noreply.github.com>
2023-11-15 11:36:29 +0100

    Create TC_2something.js

---
+++ Subfolder/TC_2something.js
@@ -0,0 +1,20 @@
+/*TC**********************
+ * @name        aTestCase
+ * @desc        This is a simple multiline
+ *              Test description
+ * 
+ * @qacTestType Smoke Test
+ * @qacProject  SuitePublish
+ * 
+ * @priority    1-high
+ *************************/
+function aTestCase() {
+    // Some Test code
+}
+
+/*TC**********************
+ * @name        anotherTestCase
+ *************************/
+function anotherTestCase() {
+    // Some Test code
+}
